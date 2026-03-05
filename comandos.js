a03081dd268a543ca652fd5a162c64cecaf9d581
kaualopes-best <kaua.lopes@ba.estudante.senai.br>
2026-03-05 14:48:15 -0300

    kk

---
+++ comandos.js
@@ -0,0 +1,14 @@
+let opisao = 3
+
+switch(opisao){
+    case 1:
+        console.log("opção 1")
+        break;
+    case 2:
+        console.log("opção 2")
+        break;
+    case 3:
+        console.log("opção 3 ")
+        break;
+
+}
